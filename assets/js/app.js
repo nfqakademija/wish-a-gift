ab50d87f4f194e85f7136e2ea48e779084972077
Olga Zyk <olga.zyk.sln@gmail.com>
2018-04-14 19:58:17 +0300

    Add variables and styles to css; Add template for secondPage

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,3 @@ require('bootstrap');
 
 
 
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,4 +16,5 @@ class HomeController extends Controller
             'controller_name' => 'HomeController',
         ]);
     }
+
 }
