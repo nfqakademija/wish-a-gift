3ce2a39e3a9b36680b87f3b7507689d046a32c15
nejuras <nerusha@gmail.com>
2018-04-19 12:44:43 +0300

    add src

---
+++ src/Entity/Gift.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GiftRepository")
+ */
+class Gift
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userId;
+
+    /**
+     * @ORM\Column(type="string", length=254)
+     */
+    private $giftName;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getGiftName(): ?string
+    {
+        return $this->giftName;
+    }
+
+    public function setGiftName(string $giftName): self
+    {
+        $this->giftName = $giftName;
+
+        return $this;
+    }
+}
