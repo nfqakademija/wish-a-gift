c9346892199082a0cd9b8483bd9186a01492af40
nejuras <nerusha@gmail.com>
2018-04-21 15:40:01 +0300

    update insert into database

--- src/Controller/WishaGiftController.php
+++ src/Controller/WishaGiftController.php
@@ -3,11 +3,11 @@ namespace App\Controller;
 
 use App\Form\GiftListType;
 use App\Entity\GiftList;
+use App\Entity\Gift;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 
 class WishaGiftController extends Controller
 {
@@ -19,19 +19,33 @@ class WishaGiftController extends Controller
     public function create(Request $request)
     {
         $giftlist = new GiftList();
-        //var_dump($request);
+        $gift = new Gift();
 
-        $form = $this->createForm(GiftListType::class, $giftlist, array(
-            'method' => 'POST'));
+        $form = $this->createForm(GiftListType::class, $giftlist);
         $form->handleRequest($request);
+
         if ($form->isSubmitted()) {
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($giftlist);
             $entityManager->flush();
 
-            return $this->redirectToRoute('publiclist');
+            $lastGiftListId = $giftlist->getId();
+
+            $entityManager1 = $this->getDoctrine()->getManager();
+
+            $data = $request->request->all();
+            var_dump($data);
+            $giftName = $data['gift_list']['Gift']['Gift'];
+
+            $gift->setUserId($lastGiftListId);
+            $gift->setGift($giftName);
+            $entityManager1->persist($gift);
+            $entityManager1->flush();
+
+           // return $this->redirectToRoute('publiclist');
         }
+
         return $this->render(
             'create/index.html.twig',
             array('form' => $form->createView())
