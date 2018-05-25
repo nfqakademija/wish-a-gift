722f4cd296cc595e2cf1081a9656aaa3781a6438
nejuras <nerusha@gmail.com>
2018-05-25 18:54:07 +0300

    unused App

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,19 @@
 
 namespace App\Controller;
 
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use App\Form\GiftListType;
-use App\Form\GiftType;
 use App\Entity\GiftList;
-use App\Entity\Gift;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
-     * @param Request $request
      * @return Response
      */
-    public function index(Request $request)
+    public function index()
     {
         $giftLists = $this->getDoctrine()
             ->getRepository(GiftList::class)
