d242dd6c0372f7bbe83c5007ab7ae1388b0b4744
Olga Zyk <olga.zyk.sln@gmail.com>
2018-06-02 06:08:18 +0300

    Update frontend dependencies; Workaround for UglifyJs bug 'Unexpected token: name (self) [js/app.a397f3b6b537d53d1872.js:10393,12]'

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,8 @@
 //add bootstrap
 require('bootstrap');
-require('amazon-autocomplete/src/amazon-autocomplete');
-
-//move images to public/images folder and add them to the manifest.json file
-// const imagesContext = require.context('../images', false, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
-// imagesContext.keys().forEach(imagesContext);
+// workaround for (possible) webpack bug to prevent error from UglifyJs 'Unexpected token: name (self) [js/app.2b7036a4c723ac53590a.js:12552,12]'
+// require('amazon-autocomplete/src/amazon-autocomplete');
+require('amazon-autocomplete/dist/amazon-autocomplete.min');
 
 //enable Jquery
 const $ = require('jquery');
