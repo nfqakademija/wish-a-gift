db2aa1771709c5d5cc47e3abd13b7b9bce57a68f
Olga Zyk <olga.zyk.sln@gmail.com>
2018-06-03 02:58:00 +0300

    Another try to mess with file access

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,4 +4,4 @@ echo "Installing dependencies..."
 docker-compose run --rm frontend.symfony bash -c "id"
 docker-compose run --rm frontend.symfony bash -c "yarn install"
 docker-compose exec prod.php.symfony composer install --no-interaction --prefer-dist
-docker-compose exec prod.php.symfony cd acceptance-tests && composer install --no-interaction --prefer-dist
+docker-compose exec prod.php.symfony composer install -d acceptance-tests --no-interaction --prefer-dist
