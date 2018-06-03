ae7ef41bae56639c3d950b398ae669347d5455f9
Olga Zyk <olga.zyk.sln@gmail.com>
2018-06-03 05:38:55 +0300

    Fix acceptance test running step

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Installing dependencies..."
-docker-compose run --rm frontend.symfony bash -c "id"
 docker-compose run --rm frontend.symfony bash -c "yarn install"
 docker-compose exec prod.php.symfony composer install --no-interaction --prefer-dist
 docker-compose exec prod.php.symfony composer install -d acceptance-tests --no-interaction --prefer-dist
--- load-fixtures.sh
+++ load-fixtures.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Loading fixtures..."
-docker-compose exec prod.php.symfony bin/console doc:fixtures:load --no-interaction
+docker-compose exec prod.php.symfony bin/console doctrine:fixtures:load --no-interaction
