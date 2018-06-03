ce6d6b2c8c3966ee7b67af997c3e5eb609c48feb
Olga Zyk <olga.zyk.sln@gmail.com>
2018-06-03 16:38:37 +0300

    Add selenium tests

--- acceptance-tests/tests/CreateGiftListFormCest.php
+++ acceptance-tests/tests/CreateGiftListFormCest.php
@@ -1,18 +1,18 @@
 <?php
 
 
-//class CreateGiftListFormCest
-//{
-//    public function createGiftList(AcceptanceTester $I)
-//    {
-//        $I->amOnPage('/create');
-//        $I->fillField('#gift_list_firstName', 'Hulk');
-//        $I->fillField('#gift_list_email', 'hulk@email.com');
-//        $I->fillField('#gift_list_title', 'Party');
-//        $I->fillField('#gift_list_description', 'Some description');
-//        $I->seeElement('.btn');
-//        $I->seeCheckboxIsChecked('gift_list[gifts][0][reservable]');
-//        $I->clickWithLeftButton('#gift_list_Save');
-//        $I->moveForward();
-//    }
-//}
+class CreateGiftListFormCest
+{
+    public function createGiftList(AcceptanceTester $I)
+    {
+        $I->amOnPage('/create');
+        $I->fillField('#gift_list_firstName', 'Hulk');
+        $I->fillField('#gift_list_email', 'hulk@email.com');
+        $I->fillField('#gift_list_title', 'Party');
+        $I->fillField('#gift_list_description', 'Some description');
+        $I->seeElement('.btn');
+        $I->seeCheckboxIsChecked('gift_list[gifts][0][reservable]');
+        $I->clickWithLeftButton('#gift_list_Save');
+        $I->moveForward();
+    }
+}
